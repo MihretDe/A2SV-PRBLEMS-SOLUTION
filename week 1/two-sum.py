dd5eb36e0a4ecea5a6e90678c96dd8b9265045fc
MihretDe <124504706+MihretDe@users.noreply.github.com>
2023-12-12 16:26:21 +0300

    Added solution for Two Sum

--- week 1/two-sum.py
+++ week 1/two-sum.py
@@ -1,17 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums_dict = {}
         for i, num in enumerate(nums):
             if target - num in nums_dict:
                 return [nums_dict[target - num], i]
             nums_dict[num] = i
         return []
-
-
-                    
+        
