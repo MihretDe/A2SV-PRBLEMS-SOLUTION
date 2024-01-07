474d56f0e63f641a22362be50c7b964022a7754a
MihretDe <124504706+MihretDe@users.noreply.github.com>
2024-01-07 12:49:46 +0300

    Added solution for Sort Colors

--- solutions/sort-colors.py
+++ solutions/sort-colors.py
@@ -3,7 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(1,len( nums)):
-            for j in range(i,0,-1):
-                if nums[j] <  nums[j-1]:
-                    nums[j],nums[j-1]=nums[j-1],nums[j]
+        # for i in range(1,len( nums)):
+        #     for j in range(i,0,-1):
+        #         if nums[j] <  nums[j-1]:
+        #             nums[j],nums[j-1]=nums[j-1],nums[j]
+        left=0
+        while left < len(nums):
+            if nums[left] ==0:
+                left+=1
+                continue
+            else:
+                right=left+1
+                while nums[left]!=[0] and right<len(nums):
+                    if nums[right] < nums[left]:
+                        nums[right],nums[left]=nums[left], nums[right]
+                    right+=1
+            left+=1
+            
